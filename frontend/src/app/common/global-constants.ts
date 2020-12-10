10c14978f6b89c2684231024fa9f357a01c7e92d
Zaldrick <zaldrick@outlook.fr>
2020-12-10 18:49:25 +0100

    update global constant for local env

--- frontend/src/app/common/global-constants.ts
+++ frontend/src/app/common/global-constants.ts
@@ -1,5 +1,5 @@
 export class GlobalConstants {
-    //public static apiURL: string = "http://localhost:3000";
-    public static apiURL: string = "https://a6ff02b0e303.ngrok.io";
+    public static apiURL: string = "http://localhost:3000";
+    //public static apiURL: string = "https://a6ff02b0e303.ngrok.io";
     
 }

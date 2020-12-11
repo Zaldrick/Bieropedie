a95b1e5981df4e16a2fc1d2d9f722dc411425e4b
Zaldrick <zaldrick@outlook.fr>
2020-12-11 10:04:39 +0100

    fix maj Controllers

---
+++ backend/controllers/notes.js
@@ -0,0 +1,99 @@
+const Note = require('../models/note');
+const fs = require('fs');
+
+exports.createNote = (req, res, next) => {
+ //const noteObject = JSON.parse(req.body);
+  //delete noteObject._id;
+  const note = new Note({
+  biereId:  req.body.biereId,
+  userId: req.body.userId,
+  notePackaging: req.body.notePackaging,
+  remarquePackaging: req.body.remarquePackaging,
+
+  noteOdeur:req.body.noteOdeur,
+  remarqueOdeur:req.body.remarqueOdeur,
+  
+  noteMiseEnBouche:req.body.noteMiseEnBouche,
+  remarqueMiseEnBouche: req.body.remarqueMiseEnBouche,
+  
+  noteRetour:req.body.noteRetour,
+  remarqueRetour: req.body.remarqueRetour,
+  
+  noteEndurance:req.body.noteEndurance,
+  remarqueEndurance:req.body.remarqueEndurance
+  });
+  note.save()
+    .then(() => res.status(201).json({ message: 'Note ajouté !'}))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.getOneNote = (req, res, next) => {
+  Note.findOne({
+    _id: req.params.id
+  }).then(
+    (note) => {
+      res.status(200).json(note);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+};
+exports.getAllNotes = (req, res, next) => {
+  Note.find().then(
+    (notes) => {
+      res.status(200).json(notes);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+
+
+exports.getAllNotesByBiere = (req, res, next) => {
+  Note.find({
+    biereId: req.params.id
+  }).then(
+    (note) => {
+      res.status(200).json(note);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+};
+/*
+exports.modifyNote = (req, res, next) => {
+  const noteObject = req.file ?
+    {
+      ...JSON.parse(req.body.note)
+    } : { ...req.body };
+  Note.updateOne({ _id: req.params.id }, { ...noteObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Note modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.deleteNote = (req, res, next) => {
+  Note.findOne({ _id: req.params.id })
+    .then(note => {
+      const filename = note.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Note.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Note supprimé !'}))
+          .catch(error => res.status(400).json({ error }));
+      });
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+*/
---
+++ backend/controllers/user.js
@@ -0,0 +1,48 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+      .then(hash => {
+        const user = new User({
+          name: req.body.name,
+          password: hash
+        });
+        user.save()
+          .then(() => res.status(201).json({ message: 'Buveur créé !' }))
+          .catch(error => {
+            console.log(error);
+            res.status(400).json({ error });
+            });
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(400).json({ error });
+    });
+};
+
+exports.login = (req, res, next) => {
+User.findOne({ name: req.body.name })
+    .then(user => {
+    if (!user) {
+        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+    }
+    bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+        if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+        }
+        res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+            { userId: user._id },
+            'RANDOM_TOKEN_SECRET',
+            { expiresIn: '48h' }
+            )
+        });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
